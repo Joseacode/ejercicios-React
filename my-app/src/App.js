773fc6fd3f65f70a16ed1b7e5eb7eaf50e7a2558
José Antonio <josea_@hotmail.com>
2021-10-18 11:16:42 -0300

    Agregando Fragment

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Eventos from "./components/Eventos.jsx";
 import Parrafo from "./components/Parrafo.jsx"
 import Variables from "./components/Variables.jsx";
+import { Fragment } from "react";
+
+//La etiqueta <Fragment> Reemplaza la etiqueta <div>  y es equivalente a <></>
+//Si se usa la útima forma no es necesario importar de React a Fragment
+
 
 function App() {
-  return (
-    <div>
-          <h1>Hola Munndo React</h1>
-          <Parrafo />
-          <Variables />
-          <Eventos />
 
-      </div>
+    return (
+
+        <Fragment>
+            <h1>Hola Munndo React</h1>
+            <Parrafo />
+            <Variables />
+            <Eventos />
+        </Fragment>
   );
 }
 
