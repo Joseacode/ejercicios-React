94cdd2f0e0f2deaaba3d3b81e271df802d29746a
José Antonio <josea_@hotmail.com>
2021-10-18 18:32:04 -0300

    Evento onChangue

--- my-app/src/components/Formulario.jsx
+++ my-app/src/components/Formulario.jsx
@@ -1,6 +1,10 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 const Formulario = () => {
+
+    const [fruta, setFruta] = useState("")
+    const [descripcion, setDescripcion] = useState("")
+
     return (
         <Fragment>
             <p>Formulario</p>
@@ -9,11 +13,13 @@ const Formulario = () => {
                     type="text"
                     placeholder="Ingrese Fruta"
                     className="form-control mb-2"
+                    onChange={(e) => (setFruta(e.target.value))}
                 />
                 <input
                     type="text"
                     placeholder="Ingrese descripcion"
                     className="form-control mb-2"
+                    onChange={(e) => (setDescripcion(e.target.value))}
                 />
                 <button className="btn btn-primary btn-block">Agregar</button>
             </form>
