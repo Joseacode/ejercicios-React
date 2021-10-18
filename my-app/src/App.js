b47b358169dded5dde5dfdedac47ca5b0886c3f5
José Antonio <josea_@hotmail.com>
2021-10-18 10:41:38 -0300

    Creando un componente

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
-
+import Parrafo from "./components/Parrafo.jsx"
 function App() {
   return (
     <div>
-      <h1>Hola Munndo React</h1>
+          <h1>Hola Munndo React</h1>
+          <Parrafo />
+          <Parrafo />
+          <Parrafo />
     </div>
   );
 }
---
+++ my-app/src/components/Parrafo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Parrafo = () => {
+    return (
+        <div>
+            <p>Bienvenidos a mi sitio Web</p>
+        </div>
+    )
+}
+export default Parrafo
