2ec518014b5ec1246e9608110df1e5af4f77798d
José Antonio <josea_@hotmail.com>
2021-10-18 10:55:49 -0300

    React Agregando constantes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Parrafo from "./components/Parrafo.jsx"
+import Variables from "./components/Variables.jsx";
+
 function App() {
   return (
     <div>
           <h1>Hola Munndo React</h1>
           <Parrafo />
-          <Parrafo />
-          <Parrafo />
-    </div>
+          <Variables />
+      </div>
   );
 }
 
---
+++ my-app/src/components/Variables.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Variables = () => {
+
+    const saludo = "Hola desde constante"
+    const imagen = "https://i.pinimg.com/564x/b9/cc/4e/b9cc4eabf9513065426eaa05a7520e96.jpg"
+    return (
+        <div>
+            <h2>Nuevo Componente {saludo}</h2>
+            <img src={ imagen } />
+        </div>
+    )
+}
+export default Variables
