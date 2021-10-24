ea2e834c46bc9c2a65dbdfe6af64f6a8178efaaa
José Antonio <josea_@hotmail.com>
2021-10-24 16:46:54 -0300

    Router useEfect

--- router-2/src/components/Nosotros.jsx
+++ router-2/src/components/Nosotros.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React from 'react'
+
 
 const Nosotros = () => {
+
+    const [equipo, setEquipo] = React.useState([])
+
+    React.useEffect(() => {
+        obtenerDatos()
+    }, [])
+
+    const obtenerDatos = async () => {
+        const data = await fetch('https://jsonplaceholder.typicode.com/users')
+        const user = await data.json()
+        setEquipo(user)
+    }
+
     return (
-    <h1>Nosotros</h1>
-    );
+        <div>
+            <h1>Nosotros</h1>
+            <ul>
+                {
+                    equipo.map(item => (
+                        <li key='item.id'> {item.name} - {item.email}</li>
+
+                        )
+                    )
+                }
+            </ul>
+        </div>
+    )
 }
 
 export default Nosotros;
